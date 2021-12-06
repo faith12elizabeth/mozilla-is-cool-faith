07c487c359fcbbff91e9fa0c2c458cf54ed4db8e
Glitch (glitch-blank-website) <none>
2021-12-06 11:49:21 +0000

    🛬🚶 Checkpoint ./scripts/main.js:39186358/947 ./index.html:39186358/8740

--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,13 @@ myHeading.textContent = "Hello world!";
 document.querySelector("html").onclick = function() {
   alert("Ouch! Stop poking me!");
 };
-let myImage = document.querySelector('img');
+let myImage = document.querySelector("img");
 
 myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'assets/firefox-icon.png') {
-      myImage.setAttribute('assets/download.jpeg');
-    } else {
-      myImage.setAttribute('firefox-icon.png');
-    }
-}
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "assets/firefox-icon.png") {
+    myImage.setAttribute("assets/download.jpeg");
+  } else {
+    myImage.setAttribute("assets/firefox-icon.png");
+  }
+};
