ec87ea32c7734233ec1d2ee415d0bc11d48bc169
Glitch (glitch-blank-website) <none>
2021-12-06 11:15:57 +0000

    ⛲️🏜 Checkpoint ./.glitch-assets:39186358/367 ./scripts/main.js:39186358/1339

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,15 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
-}
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+document.querySelector("html").onclick = function() {
+  alert("Ouch! Stop poking me!");
+};
+let myImage = document.querySelector("img");
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "assets/firefox-icon.png") {
+    myImage.setAttribute("src", "assets/download.jpeg");
+  } else {
+    myImage.setAttribute("src", "assets/firefox-icon.png");
+  }
+};
