b88d42a6649126fec73a3254b43823e55e9312aa
Glitch (glitch-blank-website) <none>
2021-12-06 11:27:04 +0000

    🚊👩 Checkpoint ./scripts/main.js:39186358/2638 ./.glitch-assets:39186358/932

--- scripts/main.js
+++ scripts/main.js
@@ -7,9 +7,12 @@ let myImage = document.querySelector("img");
 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute("src");
-  if (mySrc === "assets/firefox-icon.png") {
-    myImage.setAttribute("src", "assets/download.jpeg");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute(
+      "src",
+      "images/https://cdn.glitch.me/e9a7366c-efb4-4ea1-b562-5aca3fce89cb%2F98933491-9bd3-4e13-88fd-4c9a88c8510f.image.png?1638789901742"
+    );
   } else {
-    myImage.setAttribute("src", "assets/firefox-icon.png");
+    myImage.setAttribute("src", "images/firefox-icon.png");
   }
 };
