57111ca52fba5050751c20284c60e17641178fab
Glitch (glitch-blank-website) <none>
2021-12-06 11:38:14 +0000

    🌆🚆 Checkpoint ./scripts/main.js:39186358/811 ./.glitch-assets:39186358/595

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,13 @@ myHeading.textContent = "Hello world!";
 document.querySelector("html").onclick = function() {
   alert("Ouch! Stop poking me!");
 };
-let myImage = document.querySelector("img");
+let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
-  let mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute(
-      "src",
-      "images/https://cdn.glitch.me/e9a7366c-efb4-4ea1-b562-5aca3fce89cb%2F98933491-9bd3-4e13-88fd-4c9a88c8510f.image.png?1638789901742"
-    );
-  } else {
-    myImage.setAttribute("src", "images/firefox-icon.png");
-  }
-};
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'assets/firefox-icon.png') {
+      myImage.setAttribute('assets/download.jpeg');
+    } else {
+      myImage.setAttribute('firefox-icon.png');
+    }
+}
